7d9d64ec419dbc5e0e59d3ff94ff3d0b9c66e281
Patrick Daniel <patrickhdaniel@outlook.com>
2020-10-11 21:07:51 -0500

    login menu finished

--- WebApplication2X/ClientApp/src/components/api-authorization/LoginMenu.tsx
+++ WebApplication2X/ClientApp/src/components/api-authorization/LoginMenu.tsx
@@ -1,50 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import authService from './AuthorizeService';
 import { ApplicationPaths } from './ApiAuthorizationConstants';
 
-export class LoginMenu extends Component {
-    constructor(props) {
-        super(props);
+export function LoginMenu() {
+    const [state, setState] = useState({ isAuthenticated: false, userName: "" });
 
-        this.state = {
-            isAuthenticated: false,
-            userName: null
-        };
-    }
+    useEffect(() => {
+        var _subscription = authService.subscribe(() => populateState());
+        populateState();
+        return () => {
+            authService.unsubscribe(_subscription);
+        }
+    }, [])
 
-    componentDidMount() {
-        this._subscription = authService.subscribe(() => this.populateState());
-        this.populateState();
-    }
-
-    componentWillUnmount() {
-        authService.unsubscribe(this._subscription);
-    }
-
-    async populateState() {
+    async function populateState() {
         const [isAuthenticated, user] = await Promise.all([authService.isAuthenticated(), authService.getUser()])
-        this.setState({
+        setState({
             isAuthenticated,
             userName: user && user.name
         });
     }
 
-    render() {
-        const { isAuthenticated, userName } = this.state;
-        if (!isAuthenticated) {
-            const registerPath = `${ApplicationPaths.Register}`;
-            const loginPath = `${ApplicationPaths.Login}`;
-            return this.anonymousView(registerPath, loginPath);
-        } else {
-            const profilePath = `${ApplicationPaths.Profile}`;
-            const logoutPath = { pathname: `${ApplicationPaths.LogOut}`, state: { local: true } };
-            return this.authenticatedView(userName, profilePath, logoutPath);
-        }
-    }
-
-    authenticatedView(userName, profilePath, logoutPath) {
+    function authenticatedView(userName: string, profilePath: string, logoutPath: any) {
         return (<Fragment>
             <NavItem>
                 <NavLink tag={Link} className="text-dark" to={profilePath}> Hello {userName} </NavLink>
@@ -56,7 +35,7 @@ export class LoginMenu extends Component {
 
     }
 
-    anonymousView(registerPath, loginPath) {
+    function anonymousView(registerPath: string, loginPath: string) {
         return (<Fragment>
             <NavItem>
                 <NavLink tag={Link} className="text-dark" to={registerPath}>Register</NavLink>
@@ -65,5 +44,16 @@ export class LoginMenu extends Component {
                 <NavLink tag={Link} className="text-dark" to={loginPath}>Login</NavLink>
             </NavItem>
         </Fragment>);
+    }
+
+    const { isAuthenticated, userName } = state;
+    if (!isAuthenticated) {
+        const registerPath = `${ApplicationPaths.Register}`;
+        const loginPath = `${ApplicationPaths.Login}`;
+        return anonymousView(registerPath, loginPath);
+    } else {
+        const profilePath = `${ApplicationPaths.Profile}`;
+        const logoutPath = { pathname: `${ApplicationPaths.LogOut}`, state: { local: true } };
+        return authenticatedView(userName, profilePath, logoutPath);
     }
 }
